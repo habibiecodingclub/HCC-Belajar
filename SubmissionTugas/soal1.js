bd372a0be29ceea62c8a849f41e94dfd20516d09
sawallaz <muhammadsyawal512.sma.belajar.id>
2025-03-29 00:17:23 +0800

    submit tugas 1 dan 2

--- SubmissionTugas/soal1.js
+++ SubmissionTugas/soal1.js
@@ -25,6 +25,20 @@
 //isi algoritma mu disini (AWAS KALO GA DI ISI!!!!)
 
 
-let nama = "ini", peran = "contoh";
+let nama = "awa", peran = "Penyihir";
+
+if (nama === "") {
+    console.log("Nama wajib diisi");
+} else if (peran === "") {
+    console.log("Pilih Peranmu untuk memulai game");
+} else if (peran === "Ksatria") {
+    console.log(`Halo Ksatria ${nama}, kamu dapat menyerang dengan senjatamu!`);
+} else if (peran === "Tabib") {
+    console.log(`Halo Tabib ${nama}, kamu akan membantu temanmu yang terluka`);
+} else if (peran === "Penyihir") {
+    console.log(`Halo Penyihir ${nama}, ciptakan keajaiban yang membantu kemenanganmu!`);
+} else {
+    console.log("Tapi kayaknya kamu jadi bot aja ya, peran yang kamu pilih ga ada");
+}
 
 //code disini gunakan console.log untuk outputnya
