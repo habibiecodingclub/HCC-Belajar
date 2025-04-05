769794d2197b803a8b1b396e8373592003267840
codebaradev <bahnasep@gmail.com>
2025-04-05 20:34:22 +0800

    DamarKandi: Menyelesaikan soal2.js

--- SubmissionTugas/soal2.js
+++ SubmissionTugas/soal2.js
@@ -19,3 +19,44 @@ let tahun = 2001; // assign nilai variabel tahun disini! (dengan angka antara 19
 // console.log("Sekarang adalah tanggal 12 Bulan Februari tahun 2001")
 
 //code switch case kamu disini
+
+switch (bulan) {
+    case '1':
+        bulan = "Januari";
+        break;
+    case '2':
+        bulan = "Februari";
+        break;
+    case '3':
+        bulan = "Maret";
+        break;
+    case '4':
+        bulan = "April";
+        break;
+    case '5':
+        bulan = "Mei";
+        break;
+    case '6':
+        bulan = "Juni";
+        break;
+    case '7':
+        bulan = "Juli";
+        break;
+    case '8':
+        bulan = "Agustus";
+        break;
+    case '9':
+        bulan = "September";
+        break;
+    case '10':
+        bulan = "Oktober";
+        break;
+    case '11':
+        bulan = "November";
+        break;
+    case '12':
+        bulan = "Desember";
+        break;
+}
+
+console.log(`Sekarang adalah tanggal ${tanggal} Bulan ${bulan} tahun ${tahun}`)
